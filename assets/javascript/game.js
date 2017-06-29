e3a933ec418da1a6d5c772b52d44c3c98318a9ef
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-06-28 20:23:01 -0400

    Added pseudocode to javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//Get random target number
+
+//Get crystal value/number options
+
+//Create click event
+
+//Get value of clicked crystal
+
+//Add value to counter
+
+//Check if counter matches target number (if else)
+
+//Record wins/loses
+
+//Restart game
