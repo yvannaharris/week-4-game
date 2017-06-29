ca07119cf750eb2e0ee92cd0e54b691e97503971
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-06-28 21:55:42 -0400

    Get crystal value/number option

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,19 @@
 var targetNumber= Math.floor((Math.random() * (100 - 30)) + 30);
 console.log(targetNumber);
 
-
+$("#number-to-guess").text(targetNumber);
 
 //Get crystal value/number options
+var numberOptions= Math.floor((Math.random() * 20) + 1);
+
+var imageCrystal = $('img');
+
+imageCrystal.attr("data-crystalvalue", numberOptions);
 
 //Create click event
+$("crystal-image").on("click", function() {
+	
+})
 
 //Get value of clicked crystal
 
