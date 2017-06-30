7843013e8dae6629ff0c9a56870fa3771b53fc65
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-06-30 17:10:29 -0400

    Applied different number option to each image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,62 @@
+$(function() {
 //Get random target number
-var targetNumber= Math.floor((Math.random() * (100 - 30)) + 30);
-console.log(targetNumber);
+	var targetNumber= Math.floor((Math.random() * (100 - 30)) + 30);
+	console.log(targetNumber);
 
-$("#number-to-guess").text(targetNumber);
+	$("#number-to-guess").text(targetNumber);
 
-var counter = 0;
+
+	var counter = 0;
+
+	var win = 0;
+	var lose = 0;
 
 //Get crystal value/number options
-var numberOptions= Math.floor((Math.random() * 20) + 1);
 
-var imageCrystal = $('img');
-
-imageCrystal.attr("data-crystalvalue", numberOptions);
+$.each($('img'), function(index, value) {
+	var numberOptions = Math.floor((Math.random() * 20) + 1);
+	$(value).attr("data-crystalvalue", numberOptions);
+	console.log(numberOptions);
+});
 
 //Create click event
-$("crystal-image").on("click", function() {
+	$(".crystal-image").on("click", function() {
 
 //Get value of clicked crystal
-	var crystalValue= ($(this).attr("data-crystalvalue"));
-	crystalValue = parseInt(crystalValue);
+		var crystalValue= ($(this).attr("data-crystalvalue"));
+		crystalValue = parseInt(crystalValue);
 
 //Add value to counter	
-	counter += crystalValue;
+		counter += crystalValue;
+		console.log(counter);
+		$("#crystal-counter").text(counter);
 
 //Check if counter matches target number (if else)
-	if (counter === targetNumber) {
-		alert("You win!");
-	} else if (counter >= targetNumber) {
-		alert("You lose.");
-	}
-
-
-})
-
-
-
-
-
-
-
 //Record wins/loses
+		if (counter === targetNumber) {
+			alert("You win!");
+			win++;
+			$("#wins").text(win);
+			reset();
+		} else if (counter >= targetNumber) {
+			alert("You lose.");
+			lose++;
+			$("#loses").text(lose);
+			//reset();
+		}
+
+
+	});
+
+
+
+
 
 //Restart game
+
+
+
+
+
+});
+
